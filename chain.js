9cf46fdc098ab18ca37dd406b6c4587608389541
daringmedhruvi <70909020+daringmedhruvi@users.noreply.github.com>
2020-10-05 20:56:13 -0400

    Add files via upload

---
+++ chain.js
@@ -0,0 +1,28 @@
+class Chain {
+    constructor(bodyA, bodyB){
+      
+       var option = {
+           bodyA:bodyA,
+           bodyB:bodyB,
+           stiffness:0.04,
+           length:10
+       } 
+
+      this.chain = Matter.Constraint.create(option)
+      World.add(world, this.chain); 
+
+    }
+
+    display(){
+
+        strokeWeight(4)
+        line(this.chain.bodyA.position.x,this.chain.bodyA.position.y, this.chain.bodyB.position.x, this.chain.bodyB.position.y )
+    
+        
+
+
+
+
+    }
+
+}
